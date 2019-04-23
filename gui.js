7936c8ea3004b51f2be730b8356777c33dd46dc4
Jasmine Emanouel <40879549+jasmine-nahrain@users.noreply.github.com>
2019-04-23 18:57:13 +1000

    we fucked up

---
+++ gui.js
@@ -0,0 +1,9 @@
+
+document.getElementById("button").addEventListener('click', function() {
+  document.getElementById("g1").innerHTML = Math.random().toString();
+});
+
+
+document.getElementById("btn").addEventListener('click', function() {
+  document.getElementById("num").innerHTML = [Math().random()]*10.toString();
+});
