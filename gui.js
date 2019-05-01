b9c424fcee86242c339c6c0c76fbdc9f393c6855
jasmine-nahrain <ja5m1ne@hotmail.com>
2019-05-01 11:35:47 +1000

    First gui mostly done

--- gui.js
+++ gui.js
@@ -1,9 +1,57 @@
 
-document.getElementById("button").addEventListener('click', function() {
-  document.getElementById("g1").innerHTML = Math.random().toString();
+
+//localhost:27017 is the server host
+//exampleDB is the port -> change both once real database is created
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect("mongodb://localhost:27017/exampleDB", function(err, db) {
+  if(!err) {
+    console.log("Connection successful!");
+  }
 });
 
+void main() {
 
-document.getElementById("btn").addEventListener('click', function() {
-  document.getElementById("num").innerHTML = [Math().random()]*10.toString();
-});
+  String category;
+
+  switch (category) {
+    case "speedometer":
+      speedometer();
+      break;
+    case "acc-voltage":
+        acc_voltage();
+        break;
+    case "temperature":
+        temperature();
+        break;
+    case "steering position":
+        steering_pos();
+        break;
+    case "down-force":
+        down_force();
+        break;
+    default:
+        console.log("Didn't work");
+        break;
+  }
+}
+speedometer() {
+  var cursor = db.collection('speedometer').find({});
+   document.getElementById("speed").style.background = "green";
+}
+
+acc_voltage() {
+  var cursor = db.collection('acc-voltage').find({});
+}
+
+temperature() {
+  var cursor = db.collection('temperature').find({});
+}
+
+
+steering_pos() {
+  var cursor = db.collection('steering-pos').find({});
+}
+
+down_force() {
+  var cursor = db.collection('down-force').find({});
+}
