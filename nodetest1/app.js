9907584fc1ef7faadc0e735765a7285786a088ac
jasmine-nahrain <ja5m1ne@hotmail.com>
2019-06-12 19:32:42 +1000

    Think I'm onto something. Almost got socket io working i hope

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -1,22 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var server = require('http').Server(express);
+// var server = require('http').Server(express);
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // const io = require('socket.io')(server);
-
+// var http = require('http');
+// var server = http.createServer();
+// server.listen(3001);
 // New Code
 var monk = require('monk');
 var db = monk('localhost:27017/telemetry');
 
 var indexRouter = require('./public/javascripts/index.js');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -27,17 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
-req.db = db;
-next();
+  req.db = db;
+  next();
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-next(createError(404));
+  next(createError(404));
 });
 
 // error handler
@@ -48,7 +48,7 @@ res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 // render the error page
 res.status(err.status || 500);
-res.render('error');
+res.render('error.ejs');
 });
 
 module.exports = app;
--- nodetest1/public/javascripts/index.js
+++ nodetest1/public/javascripts/index.js
@@ -1,67 +1,69 @@
-var express = require('express');
-var router = express.Router();
-var server = require('http').Server(express);
+// var express = require('express');
+// var router = express.Router();
+
+var app = require('express')();
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var path = require('path');
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+server.listen(80);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.sendFile(__dirname, '../views/index.ejs');
- res.render('index.ejs');
-  // var db = req.db;
-  // var collection = db.get('UTSTelemetry');
-  // collection.find({}).each(( {type, number}) => {
-  //   console.log(type + " :: " + number);
-       //  res.render('index.ejs', {
-       //    speed_num: number,
-       //    type: type
-       // });
 
-  // });
+
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {"Content-Type": "text/html"});
+//     var db = req.db;
+//     var collection = db.get('UTSTelemetry');
+//     collection.find({}).each(( {type, number}) => {
+//             res.write(number);
+//         });
+//         res.end();
+//     });
+    // var path = require('path');
+
+    /* GET home page. */
+app.get('/', function(req, res, next) {
+    res.sendFile( __dirname + '/index.html');
 });
 
-server.listen(80, function(){
-  console.log('listening on *:80');
+io.on('connection', function (socket) {
+    console.log("Connected " + socket);
 });
 
+var MongoClient = require('mongodb').MongoClient;
 
-
-  /*
-    our message we want to send to the client: in this case it's just a random
-    number that we generate on the server
-  */
-  io.on('connection', function(socket) {
-    setInterval( function() {
-      socket.on('data', function(data) {
-        var data = Math.random();
-        io.emit('data', data);
-    });
-    }, 1000);
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/telemetry", function (err, db) {
+  if(err) {
+    throw err;
+  } else {
+    var db = db.db('telemetry');
+    db.collection("UTSTelemetry").find().toArray(function(err, items) {
+      if(err) throw err;
+      console.log(items);
+      io.emit('data', items);
   });
-
-  //console.log (data);
-
-
-
-// server.listen(process.env.PORT || 3000, function() {
-//   console.log('app running');
-// });
-
-/* GET mechanical page. */
-router.get('/mechanical', function(req, res) {
-    res.render('mechanical', { title: 'Mechanical' });
-});
-
-/* GET electrical page. */
-router.get('/electrical', function(req, res) {
-    res.render('electrical', { title: 'Electrical' });
+}
 });
 
 
-/* GET start up page. */
-router.get('/start-up', function(req, res) {
-    res.render('start-up', { title: 'Start Up' });
-});
+                    //
+                    // /* GET mechanical page. */
+                    // router.get('/mechanical', function(req, res) {
+                    //       res.render('mechanical', { title: 'Mechanical' });
+                    //   });
+                    //
+                    //   /* GET electrical page. */
+                    //   router.get('/electrical', function(req, res) {
+                    //         res.render('electrical', { title: 'Electrical' });
+                    //     });
+                    //
+                    //
+                    //     /* GET start up page. */
+                    //     router.get('/start-up', function(req, res) {
+                    //           res.render('start-up', { title: 'Start Up' });
+                    //       });
 
 
-module.exports = router;
+module.exports = app;
