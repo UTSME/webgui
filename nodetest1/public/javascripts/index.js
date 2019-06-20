8cf6a4f14ddd9c8552947c2b26d0bdc41b192d69
jasmine-nahrain <ja5m1ne@hotmail.com>
2019-06-20 14:24:04 +1000

    this is killing me

--- nodetest1/public/javascripts/index.js
+++ nodetest1/public/javascripts/index.js
@@ -1,14 +1,6 @@
-// var express = require('express');
-// var router = express.Router();
-
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-
-server.listen(3000, function() {
-  console.log("listening on port 3000");
-});
 
 app.get('/', function(req, res) {
     res.sendFile( __dirname + '/index.html');
@@ -77,5 +69,8 @@ app.get('/start-up', function(req, res) {
   res.render('start-up', { title: 'Start Up' });
 });
 
+server.listen(app.get('port'), function() {
+  console.log("listening on port 3000");
+});
 
 module.exports = app;
